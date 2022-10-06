5aa7bd24efa093ca3c4ead584ac3c8488f3c2a4e
BenjaminVanba <115029446+BenjaminVanba@users.noreply.github.com>
2022-10-06 19:41:34 +0200

    header php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>GBAF</title>
+
+		<link href="styleHome.css" rel="stylesheet" type="text/css">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<a href="home.php">
+			<img src="logoGBAF.jpg" alt="LogoGBAF" class="img3">
+		</a> 
+		
+		<hr class="solid">
+		<div class ="topright"> 
+
+<p><b><?=$_SESSION['name']?></b></p>
+   <p>Vous êtes bien connecté.</p>
+   <p><a href="logout.php">Se déconnecter</a></p>
+
+</div> 
+	</head>
+
+	
